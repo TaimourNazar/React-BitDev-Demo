7017867cf99ddba03bce7bafc2fa00aac0b1c7a4
Taimour <taimour.nazar1@gmail.com>
2020-10-06 12:50:18 +0500

    bit demo 1

---
+++ dist/src/components/helloworld.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Helloworld = function Helloworld() {
+  return /*#__PURE__*/_react.default.createElement("h1", null, "Hello world from bootcamp2020 class 30, its a bit.dev demo!");
+};
+
+var _default = Helloworld;
+exports.default = _default;
+
+//# sourceMappingURL=helloworld.js.map
---
+++ src/components/helloworld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Helloworld = () => (<h1>
+    Hello world from bootcamp2020 class 30, its a bit.dev demo!
+</h1>)
+
+export default Helloworld;
